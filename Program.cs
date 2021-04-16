1a17873d1156bcdbc5e0db580d0f26ec9fece4ae
hughstonrogers1 <hughstonrogers1>
2021-04-15 20:58:29 -0400

    Practice_Linq

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PracticeLinq
+{
+    class Program
+    {
+
+        private static string[] videoGames = { "Halo", "Call of Duty", "Battlefield", "Witcher", "Medal of Honor" };
+
+        static void Main(string[] args)
+        {
+
+            var num = videoGames;
+
+            var orderList = num.OrderBy(list => list.Length);
+
+            foreach (var item in orderList)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
